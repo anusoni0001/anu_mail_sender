9c766f37a64402e51d292c0e2e82276088657255
anusoni0001 <120355935+anusoni0001@users.noreply.github.com>
2023-01-04 12:01:32 -0500

    Add files via upload

---
+++ init.py
@@ -0,0 +1,14 @@
+'''
+This project send mail provied in the input.
+Mail sender service we are using is "MailSender"
+'''
+
+mail_id = str(input("Entermail id:"))
+mail_msg = str(input("Enter msg you want to send"))
+
+
+
+def send_mail(mail_id,mail_msg):
+    pass
+
+send_mail(mail_id,mail_msg)
